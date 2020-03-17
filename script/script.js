3a04e696a576ff8e7e211ce835ccc26541abaebd
Maxim Stogniy <i.dual.ce@gmail.com>
2020-03-18 02:23:36 +1200

    fix margins, borders

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger-menu__icon')
 const nav = document.querySelector('.nav')
 const hamburgerClose = document.querySelector('.hamburger-menu__close')
+const heroButton = document.querySelector('.hero__button a.btn')
 
 hamburger.addEventListener('click', (event) => {
   event.preventDefault()
@@ -10,4 +11,12 @@ hamburger.addEventListener('click', (event) => {
 hamburgerClose.addEventListener('click', (event) => {
   event.preventDefault()
   nav.classList.remove('nav--hamburger-visible') 
+})
+
+heroButton.addEventListener('mouseover', () => {
+  heroButton.classList.add('btn--green')
+})
+
+heroButton.addEventListener('mouseout', () => {
+  heroButton.classList.remove('btn--green')
 })
