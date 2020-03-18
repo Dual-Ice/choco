bfd476bfcf93adf14f110f20a845b9d17fa6572c
Maxim Stogniy <i.dual.ce@gmail.com>
2020-03-18 12:56:05 +1200

    adaptive for bars slider, js for team and menu

--- script/script.js
+++ script/script.js
@@ -2,6 +2,8 @@ const hamburger = document.querySelector('.hamburger-menu__icon')
 const nav = document.querySelector('.nav')
 const hamburgerClose = document.querySelector('.hamburger-menu__close')
 const heroButton = document.querySelector('.hero__button a.btn')
+const memberName = document.querySelectorAll('.member__name')
+const menuElementTitles = document.querySelectorAll('.menu__element-title')
 
 hamburger.addEventListener('click', (event) => {
   event.preventDefault()
@@ -20,3 +22,38 @@ heroButton.addEventListener('mouseover', () => {
 heroButton.addEventListener('mouseout', () => {
   heroButton.classList.remove('btn--green')
 })
+
+memberName.forEach(memberName => {
+  memberName.addEventListener('click', (event) => {
+    event.preventDefault()
+    const member = memberName.parentElement.parentElement
+
+    member.classList.contains('member--desc-shown') 
+    ? member.classList.remove('member--desc-shown')
+    : member.classList.add('member--desc-shown')
+  })
+})
+
+menuElementTitles.forEach(element => {
+  element.addEventListener('click', (event) => {
+    event.preventDefault()
+    const menuElement = element.parentElement
+    const close = menuElement.querySelector('.menu__element-close')
+
+    close.addEventListener('click', (event) => {
+      event.preventDefault()
+      menuElement.classList.remove('menu__element--openned')
+    })
+
+    if (menuElement.classList.contains('menu__element--openned')) {
+      menuElement.classList.remove('menu__element--openned')
+    } else {
+      const openedMenuElement = document.querySelector('.menu__element--openned')
+      if (openedMenuElement && !menuElement.isEqualNode(openedMenuElement)) {
+        openedMenuElement.classList.remove('menu__element--openned') 
+      }
+      menuElement.classList.add('menu__element--openned')
+    }
+  })
+})
+
