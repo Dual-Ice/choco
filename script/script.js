51f744c0fe95c0bfcc9af5a9e91126b80d41e26e
Maxim Stogniy <i.dual.ce@gmail.com>
2020-03-30 19:45:23 +1200

    slider done, slideshow done, form done, overlay done

--- script/script.js
+++ script/script.js
@@ -4,17 +4,29 @@ const hamburgerClose = document.querySelector('.hamburger-menu__close')
 const heroButton = document.querySelector('.hero__button a.btn')
 const memberName = document.querySelectorAll('.member__name')
 const menuElementTitles = document.querySelectorAll('.menu__element-title')
+const reviews = document.querySelectorAll('.reviews__item')
+const review = document.querySelector('.review')
+const right = document.querySelector('.arrow--right')
+const left = document.querySelector('.arrow--left')
+const barsList = document.querySelector('.bars__list')
+
+const form = document.querySelector('.order__form')
+const submitButton = document.querySelector('#submit-btn')
+
+function switchScroll () {
+  document.body.classList.toggle('body--menu-openned')
+}
 
 hamburger.addEventListener('click', (event) => {
   event.preventDefault()
   nav.classList.add('nav--hamburger-visible')
-  document.body.classList.add('body--menu-openned')
+  switchScroll()
 })
 
 hamburgerClose.addEventListener('click', (event) => {
   event.preventDefault()
   nav.classList.remove('nav--hamburger-visible') 
-  document.body.classList.remove('body--menu-openned')
+  switchScroll()
 })
 
 heroButton.addEventListener('mouseover', () => {
@@ -46,8 +58,27 @@ menuElementTitles.forEach(element => {
   element.addEventListener('click', (event) => {
     event.preventDefault()
     const menuElement = element.parentElement
-    const close = menuElement.querySelector('.menu__element-close')
+    let close
 
+    if (window.innerWidth <= 480) {
+      const section = document.querySelector('.menu')
+      const overlay = document.createElement('div')
+      overlay.classList.add('overlay')
+      overlay.innerHTML = menuElement.innerHTML
+      overlay.classList.add('menu__element--openned')
+      overlay.classList.add(menuElement.classList[2])
+      section.append(overlay)
+      close = overlay.querySelector('.menu__element-close')
+      switchScroll()
+      close.addEventListener('click', (event) => {
+        event.preventDefault()
+        section.removeChild(overlay)
+        switchScroll()
+      })
+      return
+    }
+
+    close = menuElement.querySelector('.menu__element-close')
     close.addEventListener('click', (event) => {
       event.preventDefault()
       menuElement.classList.remove('menu__element--openned')
@@ -64,4 +95,124 @@ menuElementTitles.forEach(element => {
     }
   })
 })
+
+function fillReview (reviewData) {
+  const avatar = review.querySelector('.review__avatar-img')
+  const title = review.querySelector('.review__title')
+  const text = review.querySelector('.review__text')
+  const user = review.querySelector('.review__user')
+
+  avatar.src = reviewData.avatar
+  title.textContent = reviewData.title
+  user.textContent = reviewData.user
+  text.innerHTML = reviewData.text
+}
+
+function getDataFromReview (reviewItem) {
+  return {
+    avatar: reviewItem.querySelector('.reviews__item-img').src,
+    title: reviewItem.querySelector('.reviews__item-title').textContent,
+    text: reviewItem.querySelector('.reviews__item-text').innerHTML,
+    user: reviewItem.querySelector('.reviews__item-user').textContent
+  }
+}
+
+reviews.forEach(review => {
+  review.addEventListener('click', function (e) {
+    e.preventDefault()
+    let lastActive = document.querySelector('.reviews__item--selected')
+    lastActive.classList.remove("reviews__item--selected")
+
+    lastActive = review
+    lastActive.classList.add("reviews__item--selected")
+
+    const reviewItem = getDataFromReview(review)
+    fillReview(reviewItem)
+  })
+})
+
+function loop(event, direction) {
+  event.preventDefault();
+  if (direction === "right") {
+    barsList.appendChild(barsList.firstElementChild);
+  } else {
+    barsList.insertBefore(barsList.lastElementChild, barsList.firstElementChild);
+  }
+}
+
+right.addEventListener('click', function (e) {
+  loop(e, 'right')
+})
+
+left.addEventListener('click', function (e) {
+  loop(e, 'left')
+})
+
+
+const template = document.querySelector("#modalTemplate").innerHTML;
+const modal = createModal(template);
+
+function createModal(template) {
+  const fragment = document.createElement('div');
+
+  fragment.innerHTML = template;
+
+  const modalElement = fragment.querySelector(".modal");
+  const contentElement = fragment.querySelector(".modal__content-text");
+  const closeElement = fragment.querySelector("#modal__close");
+  
+  modalElement.addEventListener("click", e => {
+    if (e.target === modalElement) {
+      closeElement.click(e);
+    }
+  });
+  closeElement.addEventListener("click", (e) => {
+    e.preventDefault()
+    document.body.removeChild(modalElement);
+    switchScroll()
+  });
+
+  return {
+    open() {
+      document.body.appendChild(modalElement);
+      switchScroll()
+    },
+    close() {
+      closeElement.click();
+    },
+    setContent(content) {
+      contentElement.innerHTML = content;
+    }
+  };
+}
+
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  const sendData = {
+    name: form.elements.name.value,
+    phone: form.elements.phone.value,
+    comment: form.elements.comment.value,
+    to: "example@mail.com"
+  }
+
+  const request = new XMLHttpRequest()
+  request.responseType = 'json'
+  request.open('POST', 'https://webdev-api.loftschool.com/sendmail')
+  let formData = new FormData()
+  for (item in sendData) {
+    formData.append(item, sendData[item])
+  }
+  request.send(formData)
+  request.addEventListener('load', () => {
+    const textContent = request.response.status 
+      ? "Сообщение отправлено"
+      : "Сообщение не отправлeно, повторите отправку позже"
+
+    modal.open();
+    modal.setContent(textContent); 
+  })
+})
+
+const reviewSelected = document.querySelector('.reviews__item--selected')
+fillReview(getDataFromReview(reviewSelected))
 
